0be22683b209cc351058cd214855ff9ea1a14fa8
Marko K. <marko.kovacevic@bvnk.com>
2024-04-09 15:40:22 +0200

    chore(NOREF-001): adds initial setup for rq and prettier

---
+++ src/providers/ReactQueryProvider.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 10,
+    },
+  },
+});
+
+export function ReactQueryProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools buttonPosition='bottom-left' initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}", "./index.html"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './index.html'],
   theme: {
     extend: {},
   },
